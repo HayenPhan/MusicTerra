a7dce1b35c235f905f775a89b9237fef47fdd8de
Hayen Phan <hayenphan@outlook.com>
2021-07-07 19:17:09 +0200

    Add multiplayer functionality.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,11 +22,67 @@ public class PlayerMovement : NetworkBehaviour
         anim = GetComponentInChildren<Animator>();
     }
 
+    private void HandleMovement() {
+            if(isLocalPlayer)
+                {
+                      Transform cameraTransform = Camera.main.gameObject.transform;  //Find main camera which is part of the scene instead of the prefab
+                        // cameraTransform.parent = CameraMountPoint.main.gameObject.transform;  //Make the camera a child of the mount point
+                        cameraTransform.position = Camera.main.gameObject.transform.position;  //Set position/rotation same as the mount point
+                        cameraTransform.rotation = Camera.main.transform.rotation;
+
+                    // Forward en Backwards
+                float moveZ = Input.GetAxis("Vertical");
+                float moveX = Input.GetAxis("Horizontal");
+
+                // x, y, z <-- used this code first
+                //moveDirection = new Vector3(moveX, 0, moveZ);
+
+                // x, y, z <-- Add camera main forward and right to move character in the direction of the camera.
+                // If you don't do this, your character will move the opposite direction, because it doesn't know the position of the camera.
+                    moveDirection = Camera.main.transform.forward * moveZ + Camera.main.transform.right * moveX;
+
+                    // Makes the player move in the right directions even when rotating with mouse
+                    // moveDirection = transform.TransformDirection(moveDirection);
+
+                    // If walkspeed is 5, moveZ will be multiplied by 5. So it walks 5 times faste
+                    // moveDirection *= walkSpeed;
+
+                    // If our movement is not equal to 0, 0, 0 then..
+                    if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+                    {
+                        // Walk
+                        Walk();
+                    }
+                    else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)){
+                        Run();
+                    }
+                    else if(moveDirection == Vector3.zero)
+                    {
+                        // Idle
+                        Idle();
+                    }
+
+                    // // Multiply direction with wanted speed
+                    moveDirection *= moveSpeed;
+
+
+                    // Time.delta -> doesn't matter how many frames you have, you will still move the same amount of time.
+                    controller.Move(moveDirection * Time.deltaTime);
+                }
+    }
+
     private void Update()
     {
-        if(Camera.main != null) {
-        Move();
-        }
+        // if(Camera.main != null) {
+        //      Move();
+        // }
+
+        // if(Camera.main != null) {
+        //     Move();
+        // }
+         if(Camera.main != null) {
+            HandleMovement();
+         }
     }
 
     private void Move()
