aa1da11fd39c5855a3c89649e1256ddc503d3520
Hayen Phan <hayenphan@outlook.com>
2022-11-04 19:07:39 +0100

    Refactor code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // // Variables
-    // [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
-    // [SerializeField] private float gravity;
+    [SerializeField] private float gravity;
 
     // // References
     private CharacterController charController;
@@ -19,58 +19,52 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 v_movement;
     private Vector3 v_velocity;
 
-    private float moveSpeed;
-    private float gravity;
-
     private void Start()
     {
         charController = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
-
-        moveSpeed = 4f;
-        gravity = 0.5f;
     }
 
-    // private void Move() {
-    //     // // Checksphere is going to draw a small sphere which checks if the player stands on the ground.
-    //     // //transform.position = player position, groundcheckdistance = radiance of the sphere, groundmask is layer check if were grounded.
-    //     // isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+    private void Move() {
+        // // Checksphere is going to draw a small sphere which checks if the player stands on the ground.
+        // //transform.position = player position, groundcheckdistance = radiance of the sphere, groundmask is layer check if were grounded.
+        // isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
 
-    //     // Keyboard input
-    //     inputX = Input.GetAxis("Horizontal");
-    //     inputZ = Input.GetAxis("Vertical");
+        // Keyboard input
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
 
-    //     // // Place keyboard input X and Z in moveDirection
-    //     // moveDirection = new Vector3(inputX, 0, inputZ);
+        // // Place keyboard input X and Z in moveDirection
+        // moveDirection = new Vector3(inputX, 0, inputZ);
 
-    //     // if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) {
-    //     //     // Walk
-    //     //     Walk();
+        // if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) {
+        //     // Walk
+        //     Walk();
 
-    //     // } else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
-    //     //     // Run
-    //     //     Run();
-    //     // } else if(moveDirection == Vector3.zero) {
-    //     //     // Idle
-    //     //     Idle();
-    //     // }
+        // } else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
+        //     // Run
+        //     Run();
+        // } else if(moveDirection == Vector3.zero) {
+        //     // Idle
+        //     Idle();
+        // }
 
-    //     // // Speed up moveDirection
-    //     // moveDirection *= moveSpeed;
+        // // Speed up moveDirection
+        // moveDirection *= moveSpeed;
 
-    //     // // Move
-    //     // charController.Move(moveDirection * Time.deltaTime * 2f);
+        // // Move
+        // charController.Move(moveDirection * Time.deltaTime * 2f);
 
-    // }
+    }
 
     private void Update()
     {
-        // Move();
-        inputX = Input.GetAxis("Horizontal");
-        inputZ = Input.GetAxis("Vertical");
+        Move();
     }
 
     private void FixedUpdate() {
+        // FixedUpdate should be used instead of update when dealing with RigidBody
+
         // input forward
         v_movement = charController.transform.forward * inputZ;
 
