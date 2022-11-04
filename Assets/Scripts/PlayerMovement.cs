1e3c24d3cb1ded84d752b51f875384e1883e109c
Hayen Phan <hayenphan@outlook.com>
2022-11-04 18:47:11 +0100

    Fix movement bug.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,91 +5,105 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // // Variables
-    [SerializeField] private float moveSpeed;
+    // [SerializeField] private float moveSpeed;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
-
-    private Vector3 moveDirection;
-    // private Vector3 velocity;
-
-    // [SerializeField] private bool isGrounded;
-    // [SerializeField] private float groundCheckDistance;
-    // [SerializeField] private LayerMask groundMask;
     // [SerializeField] private float gravity;
 
     // // References
-    private CharacterController controller;
-    private Animator anim;
+    private CharacterController charController;
+    private Animator animator;
+
+    private float inputX;
+    private float inputZ;
+    private Vector3 v_movement;
+    private Vector3 v_velocity;
+
+    private float moveSpeed;
+    private float gravity;
 
     private void Start()
     {
-            controller = GetComponent<CharacterController>();
-            anim = GetComponentInChildren<Animator>();
+        charController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        moveSpeed = 4f;
+        gravity = 0.5f;
     }
 
-    private void Move() {
-        Debug.Log("Moves");
+    // private void Move() {
+    //     // // Checksphere is going to draw a small sphere which checks if the player stands on the ground.
+    //     // //transform.position = player position, groundcheckdistance = radiance of the sphere, groundmask is layer check if were grounded.
+    //     // isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
 
-        // // Checksphere is going to draw a small sphere which checks if the player stands on the ground.
-        // //transform.position = player position, groundcheckdistance = radiance of the sphere, groundmask is layer check if were grounded.
-        // isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+    //     // Keyboard input
+    //     inputX = Input.GetAxis("Horizontal");
+    //     inputZ = Input.GetAxis("Vertical");
 
-        // Keyboard input
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+    //     // // Place keyboard input X and Z in moveDirection
+    //     // moveDirection = new Vector3(inputX, 0, inputZ);
 
-        // Place keyboard input X and Z in moveDirection
-        moveDirection = new Vector3(moveX, 0, moveZ);
+    //     // if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) {
+    //     //     // Walk
+    //     //     Walk();
 
-        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) {
-            // Walk
-            Walk();
+    //     // } else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
+    //     //     // Run
+    //     //     Run();
+    //     // } else if(moveDirection == Vector3.zero) {
+    //     //     // Idle
+    //     //     Idle();
+    //     // }
 
-        } else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
-            // Run
-            Run();
-        } else if(moveDirection == Vector3.zero) {
-            // Idle
-            Idle();
-        }
+    //     // // Speed up moveDirection
+    //     // moveDirection *= moveSpeed;
 
-        // Speed up moveDirection
-        moveDirection *= moveSpeed;
+    //     // // Move
+    //     // charController.Move(moveDirection * Time.deltaTime * 2f);
 
-        // Move
-        controller.Move(moveDirection * Time.deltaTime * 2f);
-
-        //Rotate
-    }
+    // }
 
     private void Update()
     {
-        Move();
+        // Move();
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
     }
 
+    private void FixedUpdate() {
+        // input forward
+        v_movement = charController.transform.forward * inputZ;
+
+        // char rotate
+        // WORKS
+        charController.transform.Rotate(Vector3.up * inputX * (100f * Time.deltaTime));
+
+        //char move
+        charController.Move(v_movement * moveSpeed * Time.deltaTime);
+    }
 
     // OLD
 
-    private void Idle()
-    {
-        Debug.Log("Idles");
-        anim.SetFloat("Speed", 0);
-    }
+    // private void Idle()
+    // {
+    //     Debug.Log("Idles");
+    //     animator.SetFloat("Speed", 0);
+    // }
 
-    private void Walk()
-    {
-        Debug.Log("Walks");
-        moveSpeed = walkSpeed;
+    // private void Walk()
+    // {
+    //     Debug.Log("Walks");
+    //     moveSpeed = walkSpeed;
 
-        // transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (moveDirection), Time.deltaTime * 40f);
+    //     // transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (moveDirection), Time.deltaTime * 40f);
 
-        anim.SetFloat("Speed", 0.5f);
-    }
+    //     animator.SetFloat("Speed", 0.5f);
+    // }
 
-    private void Run()
-    {
-        moveSpeed = runSpeed;
-        // // 0,1f + Time.deltaTime is going to smoothen the animation
-        anim.SetFloat("Speed", 1);
-    }
+    // private void Run()
+    // {
+    //     moveSpeed = runSpeed;
+    //     // // 0,1f + Time.deltaTime is going to smoothen the animation
+    //     animator.SetFloat("Speed", 1);
+    // }
 }
