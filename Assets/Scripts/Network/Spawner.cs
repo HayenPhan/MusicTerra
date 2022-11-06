d07179e69f7db8d4100a318fd6950f219b9ae07e
Hayen Phan <hayenphan@outlook.com>
2022-11-06 18:53:52 +0100

    Create Spawner class and add network functions.

---
+++ Assets/Scripts/Network/Spawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+
+// Handling the spawning of network objects
+public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
+{
+    // To spawn a player a prefab is needed
+    public NetworkPlayer playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void private void OnConnectedToServer(NetworkRunner runner) { Debug.Log("OnConnectedToServer"); }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        // This is what going to spawn players in the world
+        // When the player is joining and WE are the server:
+
+        if(runner.isServer)
+        {
+            Debug.Log("OnPlayerJoined we are server. Spawn player");
+            runner.Spawn(playerPrefab, Utils.GetRandomSpawnPoint(), Quaternion.identity, player);
+        }
+        else Debug.Log("OnPlayerJoined");
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        // Collect our input and send it to the network so the host can take care of it and act upon it
+
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { Debug.Log("OnShutDown"); }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { Debug.Log("OnDisconnectedFromServer"); }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { Debug.Log("OnConnectedToServer"); }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { Debug.Log("OnConnectFailed"); }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+}
